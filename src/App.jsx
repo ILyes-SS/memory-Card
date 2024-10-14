c810e0fad86640e149eec5d9fc40201ab91acffc
Senpai-cod <155851163+Senpai-cod@users.noreply.github.com>
2024-10-14 16:04:20 +0100

    Add wining logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,54 @@
 import { useState } from "react";
 import "./App.css";
 
+const cardsInfos = [
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+  {
+    title: "",
+    urlEndpoint: "",
+    id: crypto.randomUUID(),
+  },
+];
+
 function App() {
   const [bestScore, setBestScore] = useState(0);
   const [score, setScore] = useState(0);
@@ -16,6 +64,24 @@ function App() {
     if (score > bestScore) {
       setBestScore(score);
     }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dialog.close();
+  }
+
+  let dialog = (
+    <dialog open>
+      <p>You Win!!</p>
+      <button onClick={handleSubmit}>Play Again</button>
+    </dialog>
+  );
+
+  //winning
+  if (score == 9) {
+    setScore(0);
+    document.querySelector(".main").appendChild(dialog);
   }
 
   return (

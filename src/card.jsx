8d5705424ad6215627513fa73ed50b03fefcd448
Senpai-cod <155851163+Senpai-cod@users.noreply.github.com>
2024-10-19 13:12:22 +0100

    final commit

--- src/card.jsx
+++ src/card.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Card({ endpoint, title, onClick, order }) {
   let [imageSrc, setImageSrc] = useState(null);
